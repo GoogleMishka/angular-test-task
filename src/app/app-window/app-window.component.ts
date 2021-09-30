81cac8d674f4bfb153abd014b200c5c57c6f3e5a
Mihail <mishkagoo@gmail.com>
2021-09-30 19:00:57 +0300

    Creating window with 2 fields and 3 buttons (start, stop, refresh)

---
+++ src/app/app-window/app-window.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-window',
+  templateUrl: './app-window.component.html',
+  styleUrls: ['./app-window.component.scss']
+})
+export class AppWindowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppWindowComponent } from './app-window/app-window.component';
+import { StoreComponent } from './store/store.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppWindowComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule
